9bf823582ead75173fce9e8fb656b3202438c2c8
Petra Vidnerová <petra.vidnerova@gmail.com>
2018-08-08 16:54:38 +0200

    load config from file

--- nsga2_config.py
+++ nsga2_config.py
@@ -1,3 +1,6 @@
+import configparser
+import re 
+
 
 class NSGA2_Config():
 
@@ -6,5 +9,58 @@ class NSGA2_Config():
         self.input_shape = (28, 28, 1)
         self.noutputs = 10 
 
+    def load(self, filename):
+        config = configparser.ConfigParser()
+        config.read(filename)
+    
+    
+        for sec in config.sections():
+            for key, val in config[sec].items():
+                val = convert(val)
+                setattr(self, key.lower(), val)
+                setattr(self, key.upper(), val)
 
+        
 NSGA2_Cfg = NSGA2_Config()
+
+
+# auxiliary functions
+def is_int(s):
+    if re.fullmatch(r'[0-9]+', s):
+        return True
+    else:
+        return False
+
+def is_float(s):
+    if re.fullmatch(r'[0-9]+\.[0-9]+', s):
+        return True
+    else:
+        return False
+
+def is_list(s):
+    if re.fullmatch(r'\[.+\]', s):
+        return True
+    else:
+        return False
+
+def convert(s):
+    if is_int(s):
+        val = int(s)
+    elif is_float(s):
+        val = float(s)
+    elif is_list(s):
+        s = s.strip()
+        s = s.strip("[")
+        s = s.strip("]")
+        val = s.split(',')
+        newval = [] 
+        for v in val:
+            v = v.strip()
+            v = v.strip("'")
+            v = v.strip('"')
+            newval.append(convert(v))
+        val = newval
+    else:
+        val = s
+    return val
+
